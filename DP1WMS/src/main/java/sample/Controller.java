8313077a58f14e1e5b596583670aeb7d5f2c0ad3
alulab14 <alulab14@inf.pucp.edu.pe>
2017-10-18 21:05:54 -0500

    + Proyecto ejemplo

---
+++ DP1WMS/src/main/java/sample/Controller.java
@@ -0,0 +1,25 @@
+package sample;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+/**
+ *
+ * @author alulab14
+ */
+
+import javafx.event.ActionEvent;
+public class Controller {
+    public void pressButton(ActionEvent event){
+        System.out.println("Hello World");
+    }
+    
+}
