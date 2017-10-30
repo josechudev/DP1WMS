4139d8356eec63cef4bec52fa10786abbe980be4
Jose Chu <jchudb93@gmail.com>
2017-10-30 02:24:51 -0500

    productos, falta elegir categoria

--- DP1WMS/src/main/java/com/dp1wms/dao/IProducto/RepositoryMantProducto.java
+++ DP1WMS/src/main/java/com/dp1wms/dao/IProducto/RepositoryMantProducto.java
@@ -2,13 +2,14 @@ package com.dp1wms.dao.IProducto;
 
 import com.dp1wms.model.Producto;
 
+import java.text.ParseException;
 import java.util.List;
 
 public interface RepositoryMantProducto {
     List<Producto> selectAllProducto();
 
     int newIdProducto();
-    void createProducto(Producto producto);
+    void createProducto(Producto producto) throws ParseException;
     void updateProducto(Producto producto);
     void deleteProducto(Producto producto);
 }
