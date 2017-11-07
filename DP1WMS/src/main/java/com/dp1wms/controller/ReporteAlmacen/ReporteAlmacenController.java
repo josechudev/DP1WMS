dd2e7e2f8137865e020f6b759aba6494be033166
Jose <jchudb93@gmail.com>
2017-11-07 18:19:29 -0500

    + validacion fechas

--- DP1WMS/src/main/java/com/dp1wms/controller/ReporteAlmacen/ReporteAlmacenController.java
+++ DP1WMS/src/main/java/com/dp1wms/controller/ReporteAlmacen/ReporteAlmacenController.java
@@ -81,7 +81,7 @@ public class ReporteAlmacenController implements FxmlController {
 
     @FXML
     private  void consultarAlmacen(){
-<<<<<<< HEAD
+
 
         LocalDate desdeLD = this.dp_fecInicio.getValue();
         LocalDate hastaLD = this.dp_fecFin.getValue();
@@ -93,9 +93,6 @@ public class ReporteAlmacenController implements FxmlController {
         } else {
             llenarGrilla(dp_fecInicio.getValue().toString(), dp_fecFin.getValue().toString());
         }
-=======
-        llenarGrilla(dp_fecInicio.getValue().toString(),dp_fecFin.getValue().toString());
->>>>>>> 543f23b381864c001ec3349e727717c928e6b9fa
     }
     @Override
     public void initialize() {
