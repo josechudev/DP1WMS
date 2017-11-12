e3c5d79e9e50ddcb2c5b9ed04ad6e5ec31223ebc
manuel <manuel.montoya@pucp.pe>
2017-11-11 19:59:16 -0500

    algoritmo yeah

--- DP1WMS/src/main/java/com/dp1wms/controller/Tabu/AlmacenRutaController.java
+++ DP1WMS/src/main/java/com/dp1wms/controller/Tabu/AlmacenRutaController.java
@@ -4,6 +4,7 @@ import com.dp1wms.controller.FxmlController;
 
 import com.dp1wms.model.tabu.Almacen;
 import com.dp1wms.model.tabu.Producto;
+import com.dp1wms.tabu.Tabu;
 import com.dp1wms.view.StageManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -34,10 +35,37 @@ public class AlmacenRutaController implements FxmlController {
 
     @FXML
     public void click_generar_ruta(ActionEvent event){
-        System.out.println("plin");
+
+        GestorDistancias gestorDistancias = new GestorDistancias(almacen);
+        gestorDistancias.calcularDistancias();
+        int[][] distancias = gestorDistancias.getMatrizDistancia();
+        int[] caminoInicial = gestorDistancias.generarCaminoInicial();
+
+        for (int i = 0; i < caminoInicial.length ; i++) {
+            System.out.print(caminoInicial[i]);
+            System.out.print(" - ");
+        }
+        System.out.println();System.out.println();
+
+        //Ejecutar tabu
+        Tabu tabu = new Tabu(distancias, caminoInicial);
+        int[] solucion = tabu.generarCamino(1000, 1000, 5, 5);
+
+        this.imprimirAlmacen();
+
+
+        //Imprime la solucion
+        for (int i = 0; i < solucion.length ; i++) {
+            System.out.print(solucion[i]);
+            System.out.print(" - ");
+        }
+        System.out.println();System.out.println();
+
     }
 
     public void imprimirAlmacen(){
+
+        //construir grid de almacen
         for (int i = 0; i < almacen.getAlto(); i++) {
             RowConstraints row = new RowConstraints();
             row.setPrefHeight(20);
@@ -49,29 +77,58 @@ public class AlmacenRutaController implements FxmlController {
             almacen_layout.getColumnConstraints().add(col);
         }
 
-        boolean [][] layout = almacen.getAlmacen();
-        boolean [][] nodos = almacen.getNodos();
+        //color celdas
         for (int i = 0; i < almacen.getAlto(); i++) {
             for (int j = 0; j < almacen.getAncho(); j++) {
-                if(nodos[i][j] == true){
+                Region cell = new Region();
+                cell.setStyle("-fx-background-color: #B9B8FF;");
+                cell.setPrefHeight(20);
+                cell.setPrefWidth(20);
+                almacen_layout.add(cell,i,j);
+            }
+        }
+
+
+        boolean [][] layout = almacen.getAlmacen();
+        boolean [][] nodos = almacen.getNodos();
+        boolean [][] productos = almacen.getProductos();
+
+        //color racks
+        for (int i = 0; i < almacen.getAlto(); i++) {
+            for (int j = 0; j < almacen.getAncho(); j++) {
+                    if (layout[i][j]){
+                        Region cell = new Region();
+                        cell.setStyle("-fx-background-color: #4C6CCC;");
+                        cell.setPrefHeight(20);
+                        cell.setPrefWidth(20);
+                        almacen_layout.add(cell,i,j);
+                    }
+            }
+        }
+
+        //color nodos
+        for (int i = 0; i < almacen.getAlto(); i++) {
+            for (int j = 0; j < almacen.getAncho(); j++) {
+                if (nodos[i][j]){
+                    Region cell = new Region();
+                    cell.setStyle("-fx-background-color: #FFF579;");
+                    cell.setPrefHeight(20);
+                    cell.setPrefWidth(20);
+                    almacen_layout.add(cell,i,j);
+                }
+            }
+        }
+
+        //colocar productos
+        for (int i = 0; i < almacen.getAlto(); i++) {
+            for (int j = 0; j < almacen.getAncho(); j++) {
+                if (productos[i][j]){
                     Region rack = new Region();
-                    rack.setStyle("-fx-background-color: #730000;");
+                    rack.setStyle("-fx-background-color: #95CC4C;");
                     rack.setPrefHeight(20);
                     rack.setPrefWidth(20);
                     almacen_layout.add(rack,i,j);
-
-                } else{
-                    if (layout[i][j] == true){
-                        Region rack = new Region();
-                        rack.setStyle("-fx-background-color: #380B61;");
-                        rack.setPrefHeight(20);
-                        rack.setPrefWidth(20);
-                        almacen_layout.add(rack,i,j);
-                    }
-
                 }
-
-
             }
         }
 
@@ -86,7 +143,7 @@ public class AlmacenRutaController implements FxmlController {
 
         Point puntoInicio = new Point(0,0);
 
-        int numProductos = 5;
+        int numProductos = 20;
         ArrayList<Producto> productos = GestorProducto.generarProductos(almacen, numProductos);
         GestorAlmacen.llenarConProdYPtoPartida(almacen, productos, puntoInicio);
 
