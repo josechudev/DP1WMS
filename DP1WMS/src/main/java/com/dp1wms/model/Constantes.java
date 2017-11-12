a4becbec7a8143e7a96ff7379c4514096994323b
Yacs <yacs1396@gmail.com>
2017-11-12 01:20:38 -0500

    Se creo la clase constantes para poner ahi los id tipo movimiento que antes estaban hardcodeados

---
+++ DP1WMS/src/main/java/com/dp1wms/model/Constantes.java
@@ -0,0 +1,9 @@
+package com.dp1wms.model;
+
+public class Constantes {
+
+    public static final int TIPO_MOV_INGRESOLOTE = 1;
+    public static final int TIPO_MOV_SALIDA = 2;
+    public static final int TIPO_MOV_UBICACIONLOTE = 3;
+    public static final int TIPO_MOV_REUBICACIONLOTE = 4;
+}
